013c17f9158a1d4c4284ef2c6d52d5f0c2e24f0e
Jeff Schwennesen <Jeff.Schwennesen@elektrobit.com>
2019-03-01 15:44:04 -0500

    updated to pull 2019 events and only process matches that have data in TBA.   This gives OPR data in all columns because matches without data are not cleanly processed.

--- src/main/groovy/org/wlrobotics/tba/tbaEventProcessor.groovy
+++ src/main/groovy/org/wlrobotics/tba/tbaEventProcessor.groovy
@@ -8,7 +8,7 @@ class TbaEventProcessor extends Object {
   String buildDir = null
   def eventRaw = null
   TbaRestClient tba = null
-  
+
   TbaEventProcessor (String outDir, TbaRestClient t ) {
     buildDir = outDir
     this.tba = t
@@ -16,7 +16,7 @@ class TbaEventProcessor extends Object {
 
   def process(String eventKey) {
 
-    
+
     def event =  new EventData (eventKey)
     def matchFactory = new TbaMatchDataFactory(event.eventKey)
     eventRaw = this.tba.getEvent(event.eventKey)
@@ -25,7 +25,7 @@ class TbaEventProcessor extends Object {
       event.teams[t.key] = new SimpleTeamData (t)
     }
 
-    
+
     event.city = eventRaw.city
     event.city = this.eventRaw.city.toString().replace(" ","")
     tba.getMatchDataAll(event.eventKey).each { matchDataRaw ->
@@ -33,13 +33,16 @@ class TbaEventProcessor extends Object {
       //def matchDataRaw = tba.getMatchData(mk)
       // println (matchDataRaw.key)
       if (matchDataRaw.key =~ /_qm[0-9]{1,3}/) {
+
         def matchDataBlue = matchFactory.getMatch (matchDataRaw.key)
         matchDataBlue.teams = matchDataRaw?.alliances?.blue?.team_keys
         matchDataBlue.score = matchDataRaw?.alliances?.blue?.score
         matchDataBlue.color = "blue"
         matchDataBlue.matchData = matchDataRaw?.score_breakdown?.blue
         matchDataBlue.opponents_score = matchDataRaw?.alliances?.red?.score
-        event.addMatch(matchDataBlue)
+        if (matchDataBlue?.score > -1) {
+          event.addMatch(matchDataBlue)
+        }
 
         def matchDataRed = matchFactory.getMatch(matchDataRaw.key)
         matchDataRed.teams = matchDataRaw?.alliances?.red?.team_keys
@@ -47,7 +50,9 @@ class TbaEventProcessor extends Object {
         matchDataRed.opponents_score = matchDataRaw?.alliances?.blue?.score
         matchDataRed.color = "red"
         matchDataRed.matchData = matchDataRaw?.score_breakdown?.red
-        event.addMatch(matchDataRed)
+        if (matchDataRed?.score > -1) {
+          event.addMatch(matchDataRed)
+        }
       }
     }
     event.write(buildDir)
